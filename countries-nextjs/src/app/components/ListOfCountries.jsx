ab48de5a949c92aa40a65b1a6bbaa6e47d1db8f8
Rigock <rigock11@gmail.com>
2023-09-19 20:58:55 -0500

    Main page for countries with header mocked

--- countries-nextjs/src/app/components/ListOfCountries.jsx
+++ countries-nextjs/src/app/components/ListOfCountries.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import { FormatNumber } from './FormatNumber'
+import { FormatNumber } from './FormatNumber' 
+import { SearchBox } from './SearchBox' 
 
 const fetchCountries = () => {
   return fetch('https://restcountries.com/v3.1/all')
@@ -20,17 +20,18 @@ export async function ListOfCountries () {
 
   return (
     <>
-      <section className='grid'>
+    <SearchBox />
+      <section className='grid'  style={{ paddingLeft: '40px', paddingRight: '40px'}}>
         {countries.map((country) => {
           const { numericCode, population, region, capital } = country
           return <article key={numericCode}>
             <div>
-              <img style={{ borderTopLeftRadius: '5px', borderTopRightRadius: '5px'}} src={ `${country.flags.svg}`} alt={country.name.common} />
-              <div className='info'>
-                <Link href={`/country/${country.name.common}`}>
-                  <h3 style={{ margin: '8px 0px' }}>{country.name.common}</h3>
+                <Link style={{ color: '#fff', textDecoration: 'none'}} href={`/country/${country.name.common}`}>
+                  <img src={ `${country.flags.svg}`} alt={country.name.common} />
                 </Link>
-                <h4>Poplation: <span>{ FormatNumber(population) }</span></h4>
+              <div className='info'>
+                <h3 style={{ margin: '8px 0px' }}>{country.name.common}</h3>
+                <h4>Population: <span>{ FormatNumber(population) }</span></h4>
                 <h4>Region: <span>{ region }</span></h4>
                 <h4>Capital: <span>{ capital }</span></h4>
               </div>
